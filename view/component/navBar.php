cc11a3f9f8282889f012da47865813bc42200dbb
Lucas-Srz <suarez.lucas.pro@gmail.com>
2025-06-30 12:19:26 +0200

    PF25 | NavBar

--- view/component/navBar.php
+++ view/component/navBar.php
@@ -17,26 +17,3 @@
     </div>
     
 </nav>
-
-<!-- <img src="media/img/logoOrion.png" alt="Logo entreprise">
-
-<div id="navBar">
-    <a href="#" id="closeBtn">&times;</a>
-
-    <ul>
-        <li><a href="accueil.html">Accueil</a></li>
-        <li><a href="#">Qui sommes-nous ?</a></li>
-        <li><a href="#">Prestations</a></li>
-        <li><a href="#">RSE</a></li>
-        <li><a href="#">Actualités</a></li>
-        <li><a href="#">Contact</a></li>
-    </ul>
-</div>
-
-<a href="#" id="openBtn">
-    <span class="burgerIcone">
-        <span></span>
-        <span></span>
-        <span></span>
-    </span>
-</a> -->
